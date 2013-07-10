57010cd50d6945b7a5c043ff0a8c9768c7fcae21
David Luecke <daff@neyeon.com>
2013-07-10 08:48:38 -0600

    Adding Travis CI and better tests.

--- test/server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type' : 'text/plain'});
-	res.end('Echo\n');
-}).listen(1337, '127.0.0.1');
--- test/test.js
+++ test/test.js
@@ -1,65 +1,95 @@
 var expect = require('expect.js');
 var miner = require('../lib/miner.js');
-var http = require('http');
+var connect = require('connect');
+var request = require('request');
+var os = require('os');
 
-var response = 'Echo!\n';
-var port = 1337;
-// var server = require('child_process').fork('./test/server');
+describe("Miner test suite", function() {
+	var responseContent = 'Echo!\n';
+	var port = 1337;
+	var server;
 
-describe('Local', function () {
-	it('Creates a default URL', function (done) {
-		miner.local({}, function (err, url) {
-			expect(url).to.be('http://localhost');
-			done();
+	before(function(done) {
+		server = connect().use(function (req, res) {
+			res.writeHead(200, { 'Content-Type': 'text/plain' });
+			res.end(responseContent);
+		}).listen(port).on('listening', done);
+	});
+
+	after(function(done) {
+		server.close(done);
+	});
+
+	describe('Default', function () {
+		it('Creates a default URL', function (done) {
+			miner.local({}, function (err, url) {
+				expect(url).to.be('http://localhost');
+				done();
+			});
+		});
+
+		it('Uses custom port and reaches host', function (done) {
+			miner.local({ port : port }, function (error, url) {
+				expect(error).to.be.null;
+				expect(url).to.be('http://localhost:' + port);
+				request(url, function (error, response, body) {
+					expect(error).to.be.null;
+					expect(body).to.be(responseContent);
+					done();
+				});
+			});
+		});
+
+		it('Uses OS hostname', function (done) {
+			miner.local({
+				port : port,
+				useOsHostname: true
+			}, function (error, url) {
+				expect(error).to.be.null;
+				expect(url).to.be('http://' + os.hostname() + ':' + port);
+				request(url, function (error, response, body) {
+					expect(error).to.be.null;
+					expect(body).to.be(responseContent);
+					done();
+				});
+			});
 		});
 	});
 
-	it('Uses custom port and reaches host', function (done) {
-		miner.local({ port : port }, function (err, url) {
-			expect(url).to.be('http://localhost:' + port);
-//			http.request('http://localhost', function(data) {
-//				console.log(data);
-//				done();
-//			});
-			done();
+	if(process.env['TRAVIS'] !== 'true') {
+		describe('Localtunnel', function() {
+			it.skip('Opens localtunnel, returns URL', function(done) {
+				miner.localtunnel({
+					port: port
+				}, function(error, url, process) {
+					expect(error).to.be.null;
+					expect(url).to.contain('localtunnel.com');
+					request(url, function (error, response, body) {
+						expect(error).to.be.null;
+						expect(body).to.equal(responseContent);
+						process.kill();
+						done();
+					});
+				});
+			});
 		});
-	});
 
-	it.skip('Uses custom hostname and port', function () {
-
-	});
-
-	it.skip('Uses OS hostname', function () {
-
-	});
+		describe('Pagekite', function() {
+			it('Opens pagekite, returns URL', function(done) {
+				miner.pagekite({
+					name: 'miner',
+					port: port
+				}, function(error, url, process) {
+					expect(error).to.be.null;
+					expect(url).to.contain('pagekite.me');
+					request(url, function (error, response, body) {
+						expect(error).to.be.null;
+						expect(body).to.equal(responseContent);
+						process.kill();
+						done();
+					});
+				});
+			});
+		});
+	}
 });
-
-describe('Localtunnel', function() {
-	it.skip('Opens localtunnel, returns URL', function() {
-		miner.localtunnel({
-			port : 8080
-		}, function(error, url, process) {
-			console.log(arguments);
-			expect(url).to.contain('localtunnel.com');
-			expect(error).to.be(null);
-			process.kill();
-			done();
-		});
-	});
-});
-
-describe('Pagekite', function() {
-	it('Opens pagekite, returns URL', function(done) {
-		miner.pagekite({
-			name : 'daff',
-			port : 8080
-		}, function(error, url, process) {
-			expect(error).to.be(null);
-			expect(url).to.contain('pagekite.me');
-			process.kill();
-			done();
-		});
-	});
-});
-
-//describe('Showoff.io')
